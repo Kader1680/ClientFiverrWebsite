f43ce66a512b469302e0b75420c8a5ea455cdaa1
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-04-24 23:21:22 +0100

    code header

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
-// $(document).ready(function(){
-//     $(".fancybox").fancybox({
-//     openEffect: "none",
-//     closeEffect: "none"
-//     });
-//   $('#myCarousel').carousel({
-//        interval: false
-//    }); 
-
-   function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  } 
+// togggle navbar 
+let icon = document.querySelector('.fa-bars-staggered')
+let list = document.querySelector('.list')
+icon.addEventListener('click', ()=>{
+    list.classList.toggle('blk')
+})
