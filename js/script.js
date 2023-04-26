c0aa59ea62e5882d4c8120c8d4fd97889b6653d3
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-04-26 18:11:02 +0100

    add scrool animation

--- js/script.js
+++ js/script.js
@@ -8,11 +8,28 @@ icon.addEventListener('click', ()=>{
 let head = document.querySelector('.head')
 
 window.addEventListener('scroll', ()=>{
-    console.log(window.scrollY)
+    // console.log(window.scrollY)
     if (window.scrollY >= 200) {
         head.classList.add('show')
     }else{
         head.classList.remove('show')
+
+    }
+})
+
+// scroll left right animation
+let lft = document.querySelector('.lft')
+let lft_2 = document.querySelector('.lft_2')
+
+window.addEventListener('scroll', ()=>{
+    console.log(window.scrollY)
+    if (window.scrollY >= 400) {
+        lft.classList.add('block')
+        lft_2.classList.add('block')
+
+    }else{
+        lft.classList.remove('block')
+        lft_2.classList.remove('block')
 
     }
 })
