a1e3bdf5f52d3635db089c1048e5c9181e676e4a
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-04-26 13:06:56 +0100

    add header

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ let list = document.querySelector('.list')
 icon.addEventListener('click', ()=>{
     list.classList.toggle('blk')
 })
+// scroll animation
+let head = document.querySelector('.head')
+
+window.addEventListener('scroll', ()=>{
+    console.log(window.scrollY)
+    if (window.scrollY === 540) {
+        alert('gfgfdgdfg')
+        // head.classList.add('show')
+    } else {
+        // head.classList.remove('show')
+        
+    }
+})
