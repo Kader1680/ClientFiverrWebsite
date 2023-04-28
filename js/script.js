d806a33e1dd0d7f5fdf0c9ae2c57b8d49c17fb55
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-04-28 16:01:26 +0100

    add counter number

--- js/script.js
+++ js/script.js
@@ -21,18 +21,21 @@ window.addEventListener('scroll', ()=>{
 let lft = document.querySelector('.lft')
 let lft_2 = document.querySelector('.lft_2')
 
+
+
 window.addEventListener('scroll', ()=>{
     console.log(window.scrollY)
     if (window.scrollY >= 400) {
         lft.classList.add('block')
         lft_2.classList.add('block')
-
+        
     }else{
         lft.classList.remove('block')
         lft_2.classList.remove('block')
-
+        
     }
 })
+
 let rht = document.querySelector('.rht')
 let rht_2 = document.querySelector('.rht_2')
 
@@ -48,3 +51,27 @@ window.addEventListener('scroll', ()=>{
 
     }
 })
+
+// scroll number counter
+
+let counter = document.querySelector('.counter')
+let nums = document.querySelectorAll('.nums .num')
+
+
+nums.forEach((num) => {
+    startCouter(num)
+    
+});
+function startCouter(element) {
+    // define goal number
+    let goal = element.dataset.goal
+    // counter that start from input element to max
+
+    let count = setInterval(() => {
+        element.textContent++;
+        if (element.textContent == goal) {
+            clearInterval(count)
+        }
+    }, 10);
+    
+}
